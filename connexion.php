9bd3e88dafdc06bc24d16da89e1f080ef7a06dec
FADLI-Said <saidfadli224@gmail.com>
2025-03-04 11:02:33 +0100

    First commit

---
+++ connexion.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        body {
+            height: 100vh;
+        }
+
+        form {
+            width: 30%;
+            height: 100%;
+        }
+
+        button {
+            width: 100%;
+        }
+    </style>
+</head>
+
+<body class="container">
+    <form type="submit" class="text-center m-auto align-content-center" method="POST" novalidate>
+        <h1></h1>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" value="" required>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Mot de passe</label>
+            <input type="password" class="form-control" id="password" value="" required>
+        </div>
+        <button type="submit" class="btn btn-primary mb-3">Connexion</button>
+        <a href="./inscription.php">Pas encore de compte ? Inscrivez-vous !</a>
+    </form>
+
+
+    <script src="https://kit.fontawesome.com/50a1934b21.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
