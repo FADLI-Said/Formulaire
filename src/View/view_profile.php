b61075106e0e892e22b46abc66c42295cd94a840
FADLI-Said <saidfadli224@gmail.com>
2025-03-05 16:53:15 +0100

    profile finit

--- src/View/view_profile.php
+++ src/View/view_profile.php
@@ -1,15 +1,13 @@
 <?php
 
 include_once "../../templates/head.php";
-
 session_start();
 
-echo $_SESSION = $user;
-
+var_dump($_SESSION)
 ?>
 
 <body>
-    <h1 class="text-center">Bienvenu</h1>
+    <h1 class="text-center">Bienvenu <span class="text-uppercase"><?= $_SESSION["user_pseudo"] ?></span></h1>
 </body>
 
 </html>
